030a180f6c829f2bf462e98f460003fd54c2181d
Pablo Daniel Cano <pdcano@gmail.com>
2012-10-11 20:16:37 -0300

    Phrasebook format doc

---
+++ lib/SQL/Phrasebook/Format.pm
@@ -0,0 +1,40 @@
+package SQL::Phrasebook::Format;
+
+=head1 NAME
+
+SQL::Phrasebook::Format - A sample pod document
+
+=head1 SYNOPSIS
+
+    Blah
+
+=head1 DESCRIPTION
+
+
+=head2 An Example List
+
+=over 4
+
+=item * This is a bulleted list.
+
+=item * Here's another item.
+
+=back
+
+=begin html
+
+=end html
+
+=head1 SEE ALSO
+
+L<perlpod>, L<perldoc>, L<Pod::Parser>.
+
+=head1 COPYRIGHT
+
+Copyright 2012 Pablo Daniel Cano.
+
+This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+=cut
+
+1
